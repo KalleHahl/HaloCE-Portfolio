863bde736ca6ce4a6d5722732080dfbb9b05a47b
Kalle Hahl <kalle.hahl@hotmail.com>
2024-02-10 16:11:08 +0200

    added new pages for projects and experience

---
+++ portfolio/app/experience/page.tsx
@@ -0,0 +1,11 @@
+import GradientBackground from "@/components/GradientBackground"
+
+const ExperiencePage = () => {
+  return (
+    <GradientBackground text="EXPERIENCE">
+      <div></div>
+    </GradientBackground>
+  )
+}
+
+export default ExperiencePage
---
+++ portfolio/app/projects/page.tsx
@@ -0,0 +1,11 @@
+import GradientBackground from "../../components/GradientBackground"
+
+const ProjectsPage = () => {
+  return (
+    <GradientBackground text="PROJECTS">
+      <div className="flex flex-col h-96 md:flex-row md:h-full relative"></div>
+    </GradientBackground>
+  )
+}
+
+export default ProjectsPage
