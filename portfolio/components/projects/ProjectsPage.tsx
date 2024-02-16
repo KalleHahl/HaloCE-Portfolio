a8be315549ec414f6a46f323207b44e57ce3b0af
Kalle Hahl <kalle.hahl@hotmail.com>
2024-02-16 02:23:49 +0200

    Made initial carousel for project cards and made a very bootleg (or genius) way of rendering less cards on smaller screen

--- portfolio/components/projects/ProjectsPage.tsx
+++ portfolio/components/projects/ProjectsPage.tsx
@@ -4,24 +4,20 @@ import InfoCard from "../InfoCard"
 const ProjectsPage = () => {
   return (
     <div className="relative flex h-full justify-center">
-      <div className="flex justify-center items-center">
-        <Arrow direction="left" />
-        <div className="w-2/3">
-          <div className="flex w-full justify-evenly">
-            <InfoCard />
-            <InfoCard />
-            <InfoCard />
-          </div>
-        </div>
-
-        <Arrow direction="right" />
-        {/* <div className="h-0 w-0 relative lg:h-[300px] lg:w-[60px]">
-        <div className="absolute inset-0 border-b-transparent border-r-sky-700 border-t-transparent border-opacity-50 hover:border-r-white hover:border-opacity-50 lg:border-b-[300px] lg:border-r-[60px] lg:border-t-[300px]"></div>
+      <Arrow direction="left" />
+      <div className="flex w-full absolute lg:relative invisible lg:visible justify-center px-4 gap-4">
+        <InfoCard />
+        <InfoCard />
+        <InfoCard />
       </div>
-      <div className="h-0 w-0 relative lg:h-[300px] lg:w-[60px]">
-        <div className="absolute inset-0 border-b-transparent border-l-sky-700 border-t-transparent border-opacity-50 hover:border-l-white hover:border-opacity-50 lg:border-b-[300px] lg:border-l-[60px] lg:border-t-[300px]"></div>
-      </div> */}
+      <div className="flex w-full absolute invisible md:visible md:relative lg:invisible lg:absolute justify-center px-4 gap-4">
+        <InfoCard />
+        <InfoCard />
       </div>
+      <div className="flex w-ful  md:invisible md:absolute justify-center px-4 gap-4">
+        <InfoCard />
+      </div>
+      <Arrow direction="right" />
     </div>
   )
 }
