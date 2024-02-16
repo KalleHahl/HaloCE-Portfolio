5abca5e7324ba6f79893cb2b9c6961ae53ce7cf9
Kalle Hahl <kalle.hahl@hotmail.com>
2024-02-16 18:18:14 +0200

    added data for work experience and projects

---
+++ portfolio/public/data/experiences.ts
@@ -0,0 +1,14 @@
+export const experiences = [
+  {
+    title: "Prisma Olari",
+    description: "Worked at the local grocery store as a cashier and a stocker",
+    image: "prisma.png",
+    time: "2018-2020",
+  },
+  {
+    title: "Vertico Oy",
+    description: "Part time work in various roles in a grocery store",
+    image: "vertico.png",
+    time: "2020-2021",
+  },
+]
---
+++ portfolio/public/data/projects.ts
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    title: "Tetris",
+    description: "Desktop application of the classic game Tetris",
+    image: "tetris.png",
+    technologies: "Python, Pygame, Pytest, Poetry",
+  },
+  {
+    title: "Shunting Yard Calculator",
+    description:
+      "Command line application that evaluates mathematical expressions using the shunting yard algorithm",
+    image: "Shunting.png",
+    technologies: "Python, Pytest, Poetry",
+  },
+  {
+    title: "MuViCO",
+    description: "Music visualization for concerts",
+    image: "muvico.png",
+    technologies: "React, Node.js, Express, MongoDB, Docker",
+  },
+  {
+    title: "BibTex cite controller",
+    description: "Command line application that manages BibTex citations",
+    image: "bibtex.png",
+    technologies: "Python, Pytest, Poetry, Robot Framework",
+  },
+  {
+    title: "Halo: Combat Evolved -Portfolio",
+    description:
+      "Portfolio website created in the spirit of the game Halo: Combat Evolved",
+    image: "halo.png",
+    technologies: "React, Next.js, Tailwind CSS, Framer Motion, Vercel",
+  },
+]
