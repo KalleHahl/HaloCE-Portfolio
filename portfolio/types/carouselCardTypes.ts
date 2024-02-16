58b4c8a333e1b166e4e52f1f9da3e40f804c38f7
Kalle Hahl <kalle.hahl@hotmail.com>
2024-02-16 18:16:05 +0200

    added types for carousel props

---
+++ portfolio/types/carouselCardTypes.ts
@@ -0,0 +1,19 @@
+interface BaseCard {
+  title: String
+  description: String
+  image: String
+}
+
+export interface ProjectCard extends BaseCard {
+  technologies: String
+}
+
+export interface ExperienceCard extends BaseCard {
+  time: String
+}
+
+export type CarouselCard = ProjectCard | ExperienceCard
+
+export interface CarouselProps {
+  items: CarouselCard[]
+}
