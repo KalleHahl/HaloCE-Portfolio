92938e02b4b63362bf5348b2f957054aa130636b
Kalle Hahl <kalle.hahl@hotmail.com>
2024-02-16 18:20:41 +0200

    made info card into a reusable component

--- portfolio/components/InfoCard.tsx
+++ portfolio/components/InfoCard.tsx
@@ -1,7 +1,17 @@
-const InfoCard = () => {
+import {
+  CarouselCard,
+  ProjectCard,
+  ExperienceCard,
+} from "@/types/carouselCardTypes"
+
+const InfoCard = ({ item }: { item: CarouselCard }) => {
+  const project = "technologies" in item
+  const experience = "time" in item
   return (
     <div className="w-full h-full rounded-xl border-4 border-transparent bg-black bg-opacity-30 px-2 hover:border-sky-500 hover:bg-sky-900 hover:bg-opacity-30">
-      <p className="text-center text-4xl font-bold text-sky-500">Title</p>
+      <p className="text-center text-4xl font-bold text-sky-500">
+        {item.title}
+      </p>
       <div className="flex justify-center px-4">
         <div className="h-full w-full rounded-xl border-4 border-sky-700">
           <p className="my-10 text-center text-4xl font-bold text-sky-500">
@@ -10,9 +20,23 @@ const InfoCard = () => {
         </div>
       </div>
       <p className="mt-2 text-4xl font-bold text-sky-500">Description:</p>
-      <p className="ml-3 text-2xl font-bold text-sky-500">yadda yadda yaa</p>
-      <p className="mt-2 text-4xl font-bold text-sky-500">Technologies:</p>
-      <p className="ml-3 text-2xl font-bold text-sky-500">etc etc etc</p>
+      <p className="ml-3 text-2xl font-bold text-sky-500">{item.description}</p>
+      {project && (
+        <>
+          <p className="mt-2 text-4xl font-bold text-sky-500">Technologies:</p>
+          <p className="ml-3 text-2xl font-bold text-sky-500">
+            {(item as ProjectCard).technologies}
+          </p>
+        </>
+      )}
+      {experience && (
+        <>
+          <p className="mt-2 text-4xl font-bold text-sky-500">Time:</p>
+          <p className="ml-3 text-2xl font-bold text-sky-500">
+            {(item as ExperienceCard).time}
+          </p>
+        </>
+      )}
     </div>
   )
 }
