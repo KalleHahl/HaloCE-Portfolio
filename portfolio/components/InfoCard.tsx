d910d95b1fa6bfa964875ae52110b0e98f36e0c9
Kalle Hahl <kalle.hahl@hotmail.com>
2024-02-16 02:32:25 +0200

    moved docs folder to root, changed texts in infocard

--- portfolio/components/InfoCard.tsx
+++ portfolio/components/InfoCard.tsx
@@ -1,16 +1,18 @@
 const InfoCard = () => {
   return (
     <div className="w-full h-full rounded-xl border-4 border-transparent bg-black bg-opacity-30 px-2 hover:border-sky-500 hover:bg-sky-900 hover:bg-opacity-30">
-      <p className="text-center text-4xl font-bold text-sky-500">You</p>
+      <p className="text-center text-4xl font-bold text-sky-500">Title</p>
       <div className="flex justify-center px-4">
-        <div className="h-full w-full rounded-xl border-4 border-sky-700"></div>
+        <div className="h-full w-full rounded-xl border-4 border-sky-700">
+          <p className="my-10 text-center text-4xl font-bold text-sky-500">
+            Picture
+          </p>
+        </div>
       </div>
-      <p className="mt-2 text-4xl font-bold text-sky-500">Controls:</p>
-      <p className="ml-3 text-2xl font-bold text-sky-500">Default</p>
-      <p className="mt-2 text-4xl font-bold text-sky-500">Controls:</p>
-      <p className="ml-3 text-2xl font-bold text-sky-500">Default</p>
-      <p className="mt-2 text-4xl font-bold text-sky-500">Current Level:</p>
-      <p className="ml-3 text-2xl font-bold text-sky-500">Default</p>
+      <p className="mt-2 text-4xl font-bold text-sky-500">Description:</p>
+      <p className="ml-3 text-2xl font-bold text-sky-500">yadda yadda yaa</p>
+      <p className="mt-2 text-4xl font-bold text-sky-500">Technologies:</p>
+      <p className="ml-3 text-2xl font-bold text-sky-500">etc etc etc</p>
     </div>
   )
 }
