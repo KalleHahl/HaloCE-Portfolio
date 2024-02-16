7af0ef1f1d9030f5c7c8a9692c37161e7e37016a
Kalle Hahl <kalle.hahl@hotmail.com>
2024-02-16 02:22:36 +0200

    added arrwo component and enhanced responsiveness of InfoCard

---
+++ portfolio/components/Arrow.tsx
@@ -0,0 +1,29 @@
+interface TriangleProps {
+  direction: "right" | "left"
+}
+
+const Arrow = ({ direction }: TriangleProps) => {
+  const w = 40
+  const h = 400
+  const points = {
+    top: [`${w / 2},0`, `0,${h}`, `${w},${h}`],
+    right: [`0,0`, `0,${h}`, `${w},${h / 2}`],
+    bottom: [`0,0`, `${w},0`, `${w / 2},${h}`],
+    left: [`${w},0`, `${w},${h}`, `0,${h / 2}`],
+  }
+
+  const align = direction === "left" ? "justify-end" : "justify-start"
+
+  return (
+    <div className={`flex ${align} items-center w-1/5`}>
+      <svg width={w} height={h}>
+        <polygon
+          points={points[direction].join(" ")}
+          className="fill-sky-700 opacity-50 stroke-sky-500 hover:fill-white"
+        />
+      </svg>
+    </div>
+  )
+}
+
+export default Arrow
--- portfolio/components/InfoCard.tsx
+++ portfolio/components/InfoCard.tsx
@@ -1,9 +1,9 @@
 const InfoCard = () => {
   return (
-    <div className="w-[400px] rounded-xl border-4 border-transparent bg-black bg-opacity-30 px-2 hover:border-sky-500 hover:bg-sky-900 hover:bg-opacity-30">
+    <div className="w-full h-full rounded-xl border-4 border-transparent bg-black bg-opacity-30 px-2 hover:border-sky-500 hover:bg-sky-900 hover:bg-opacity-30">
       <p className="text-center text-4xl font-bold text-sky-500">You</p>
-      <div className="flex justify-center">
-        <div className="h-48 w-48 rounded-xl border-4 border-sky-700"></div>
+      <div className="flex justify-center px-4">
+        <div className="h-full w-full rounded-xl border-4 border-sky-700"></div>
       </div>
       <p className="mt-2 text-4xl font-bold text-sky-500">Controls:</p>
       <p className="ml-3 text-2xl font-bold text-sky-500">Default</p>
