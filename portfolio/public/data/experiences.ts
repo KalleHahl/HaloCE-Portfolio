72ece2c30805c76cbcaeea9517be9cff3802b225
Kalle Hahl <kalle.hahl@hotmail.com>
2024-02-17 17:23:58 +0200

    added photos and experience/project data to public folder

--- portfolio/public/data/experiences.ts
+++ portfolio/public/data/experiences.ts
@@ -1,14 +1,21 @@
+import easy from "@/public/easy.png"
+import normal from "@/public/normal.png"
+import heroic from "@/public/heroic.png"
+import legendary from "@/public/legendary.png"
+
 export const experiences = [
   {
     title: "Prisma Olari",
     description: "Worked at the local grocery store as a cashier and a stocker",
     image: "prisma.png",
     time: "2018-2020",
+    difficulty: normal,
   },
   {
     title: "Vertico Oy",
     description: "Part time work in various roles in a grocery store",
     image: "vertico.png",
     time: "2020-2021",
+    difficulty: heroic,
   },
 ]
--- portfolio/public/data/projects.ts
+++ portfolio/public/data/projects.ts
@@ -1,34 +1,50 @@
+import muvico from "@/public/muvico.jpeg"
+import tetris from "@/public/tetris.png"
+import shunting from "@/public/shunting.jpg"
+import bibtex from "@/public/bibtex.png"
+import halo1 from "@/public/halo1.jpg"
+
+import easy from "@/public/easy.png"
+import normal from "@/public/normal.png"
+import heroic from "@/public/heroic.png"
+import legendary from "@/public/legendary.png"
+
 export const projects = [
   {
     title: "Tetris",
     description: "Desktop application of the classic game Tetris",
-    image: "tetris.png",
+    image: tetris,
     technologies: "Python, Pygame, Pytest, Poetry",
+    difficulty: heroic,
   },
   {
     title: "Shunting Yard Calculator",
     description:
       "Command line application that evaluates mathematical expressions using the shunting yard algorithm",
-    image: "Shunting.png",
+    image: shunting,
     technologies: "Python, Pytest, Poetry",
+    difficulty: normal,
   },
   {
-    title: "MuViCO",
+    title: "MuViCo",
     description: "Music visualization for concerts",
-    image: "muvico.png",
+    image: muvico,
     technologies: "React, Node.js, Express, MongoDB, Docker",
+    difficulty: legendary,
   },
   {
     title: "BibTex cite controller",
     description: "Command line application that manages BibTex citations",
-    image: "bibtex.png",
+    image: bibtex,
     technologies: "Python, Pytest, Poetry, Robot Framework",
+    difficulty: easy,
   },
   {
-    title: "Halo: Combat Evolved -Portfolio",
+    title: "Halo CE -Portfolio",
     description:
       "Portfolio website created in the spirit of the game Halo: Combat Evolved",
-    image: "halo.png",
+    image: halo1,
     technologies: "React, Next.js, Tailwind CSS, Framer Motion, Vercel",
+    difficulty: legendary,
   },
 ]
