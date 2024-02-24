34bf58bc26c4b4e0f77d183a97fd6eee978a98c8
Kalle Hahl <kalle.hahl@hotmail.com>
2024-02-25 00:43:36 +0200

    added pics

--- portfolio/public/data/experiences.ts
+++ portfolio/public/data/experiences.ts
@@ -2,12 +2,14 @@ import easy from "@/public/easy.png"
 import normal from "@/public/normal.png"
 import heroic from "@/public/heroic.png"
 import legendary from "@/public/legendary.png"
+import prisma from "@/public/prisma.jpg"
+import vertico from "@/public/vertico.png"
 
 export const experiences = [
   {
     title: "Prisma Olari",
     description: "Worked at the local grocery store as a cashier and a stocker",
-    image: "prisma.png",
+    image: prisma,
     time: "2018-2020",
     url: "https://hok-elanto.fi/",
     difficulty: normal,
@@ -15,7 +17,7 @@ export const experiences = [
   {
     title: "Vertico Oy",
     description: "Part time work in various roles in a grocery store",
-    image: "vertico.png",
+    image: vertico,
     time: "2020-2021",
     url: "https://www.henkilostopartneri.fi/",
     difficulty: heroic,
