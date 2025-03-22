49278146a08317958de9b7c288cad893b4b1e29d
KalleHahl <kalle.hahl@helsinki.fi>
2025-03-22 21:15:14 +0200

    updated work information

--- portfolio/public/data/experiences.ts
+++ portfolio/public/data/experiences.ts
@@ -1,11 +1,22 @@
 import easy from "@/public/difficulties/easy.png"
 import normal from "@/public/difficulties/normal.png"
 import heroic from "@/public/difficulties/heroic.png"
+import legendary from "@/public/difficulties/legendary.png"
+
 import prisma from "@/public/workphotos/prisma.jpg"
 import vertico from "@/public/workphotos/vertico.png"
 import demande from "@/public/workphotos/demande.png"
+import wellmo from "@/public/workphotos/wellmo.png"
 
 export const experiences = [
+  {
+    title: "Mobile Wellness Solutions MWS",
+    description: "Software Developer working on frontend with TypeScript and React",
+    image: wellmo,
+    time: "2024-present",
+    url: "https://www.wellmo.com/",
+    difficulty: legendary
+  },
   {
     title: "Prisma Olari",
     description: "Worked at the local grocery store as a cashier and a stocker",
